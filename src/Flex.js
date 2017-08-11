1f9c23a3b081dd26c845a0bd5a908ce0fc32751f
Brent Jackson <jxnblk@gmail.com>
2017-08-11 10:19:36 -0400

    Adjust responsiveStyle Flex functions

--- src/Flex.js
+++ src/Flex.js
@@ -7,8 +7,8 @@ import Box from './Box'
 
 const wrap = responsiveStyle('flex-wrap', 'wrap', 'wrap')
 const direction = responsiveStyle('flex-direction', 'direction')
-const align = props => responsiveStyle('align-items', 'align')
-const justify = props => responsiveStyle('justify-content', 'justify')
+const align = responsiveStyle('align-items', 'align')
+const justify = responsiveStyle('justify-content', 'justify')
 const column = props => props.column ? `flex-direction:column;` : null
 
 const Flex = styled(Box)([],

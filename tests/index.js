555b6dc6d84159fdf47e1d533aa2fa07e4a4268a
Brent Jackson <jxnblk@gmail.com>
2018-06-22 21:11:56 -0400

    Dry up tests

--- tests/index.js
+++ tests/index.js
@@ -7,75 +7,75 @@ const renderJSON = el => render(el).toJSON()
 
 // Box
 test('Box renders', () => {
-  const json = render(<Box m={2} px={3} />).toJSON()
+  const json = renderJSON(<Box m={2} px={3} />)
   expect(json).toMatchSnapshot()
 })
 
 test('Box renders with props', () => {
-  const json = render(<Box
+  const json = renderJSON(<Box
     m={[ 1, 2 ]}
     px={[ 1, 2 ]}
     w={1}
     flex='1 1 auto'
     alignSelf='flex-start'
-  />).toJSON()
+  />)
   expect(json).toMatchSnapshot()
 })
 
 // Flex
 test('Flex renders', () => {
-  const json = render(<Flex />).toJSON()
+  const json = renderJSON(<Flex />)
   expect(json).toMatchSnapshot()
 })
 
 test('Flex renders with props', () => {
-  const json = render(
+  const json = renderJSON(
     <Flex
       flexWrap='wrap'
       flexDirection='column'
       alignItems='center'
       justifyContent='space-between'
     />
-  ).toJSON()
+  )
   expect(json).toMatchSnapshot()
 })
 
 test('Flex renders with legacy props', () => {
-  const json = render(
+  const json = renderJSON(
     <Flex
       wrap
       flexDirection='column'
       align='center'
       justify='space-between'
     />
-  ).toJSON()
+  )
   expect(json).toMatchSnapshot()
 })
 
 test('Flex renders with flexDirection prop', () => {
-  const json = render(
+  const json = renderJSON(
     <Flex
       flexDirection='column'
     />
-  ).toJSON()
+  )
   expect(json).toMatchSnapshot()
 })
 
 test('Flex renders with responsive props', () => {
-  const json = render(
+  const json = renderJSON(
     <Flex
       wrap={[ true, false ]}
       flexDirection={[ 'column', 'row' ]}
       align={[ 'stretch', 'center' ]}
       justify={[ 'space-between', 'center' ]}
     />
-  ).toJSON()
+  )
   expect(json).toMatchSnapshot()
 })
 
 // clean-tag
 test('Box removes grid-styled props', () => {
-  const json = render(
+  const json = renderJSON(
     React.createElement(Box, {
       id: 'hi',
       width: .5,
@@ -84,7 +84,7 @@ test('Box removes grid-styled props', () => {
       wrap: true,
       flexWrap: 'wrap'
     })
-  ).toJSON()
+  )
   expect(json.props.id).toBe('hi')
   expect(json.props.width).toBe(undefined)
   expect(json.props.color).toBe(undefined)
@@ -93,10 +93,10 @@ test('Box removes grid-styled props', () => {
 })
 
 test('Box accepts an is prop to change elements', () => {
-  const json = render(
+  const json = renderJSON(
     React.createElement(Box, {
       is: 'h2'
     })
-  ).toJSON()
+  )
   expect(json.type).toBe('h2')
 })

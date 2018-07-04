883b51931a054451f6544c402de2bc6a0b88db65
Brent Jackson <jxnblk@gmail.com>
2018-07-04 12:44:01 -0400

    Clean up colors

--- docs/_components.js
+++ docs/_components.js
@@ -6,21 +6,9 @@ import { Arrow } from 'reline'
 
 export const colors = {
   cyan: '#0ff',
-  blue: '#08f',
   violet: '#80f',
   magenta: '#f0f',
-  pink: '#f08',
-  orange: '#f80',
-  yellow: '#ff0',
   lime: '#8f0',
-  teal: '#0f8',
-
-  // old
-  manilla: '#f9f6d1',
-  slate: '#125b7a',
-  hi: '#c3ffb9',
-  green: '#93ff81',
-  light: '#fcfbe8'
 }
 
 const cx = key => colors[key] || key
